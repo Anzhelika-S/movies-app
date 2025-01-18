8898a9f7a8b8839eec2829a7386ab3a1a92ca7a4
Anzhelika <anzhelikasilkina@yandex.ru>
2025-01-18 12:59:50 +0300

    install dependancies fix config

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,28 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import babelParser from '@babel/eslint-parser';
 
 export default [
-  { ignores: ['dist'] },
+  { ignores:  ['node_modules', 'dist', 'build']},
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 12,
       globals: globals.browser,
+     parser: babelParser,
       parserOptions: {
+        requireConfigFile: false,
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: ['@babel/preset-react'],
+        },
       },
     },
     settings: { react: { version: '18.3' } },
@@ -22,6 +32,8 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+        import: importPlugin,
+      eslintConfigPrettier,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -32,6 +44,19 @@ export default [
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      indent: ['error', 2],
+      'linebreak-style': [0, 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 0,
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
       ],
     },
   },
